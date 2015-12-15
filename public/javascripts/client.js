151f476a52fdc8f34ab0fd2591fcf6e07a8c0f4a
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2015-12-15 16:44:31 -0600

    After some advice from joseph, refactored clientside page/logic to hopefully make page update on all clients

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -5,17 +5,19 @@ var app = angular.module('spookyScarySkeleton', ['ng-sortable']);
 var socket = io('http://localhost:3000');
 app.controller('sortable', ['$scope',function($scope){
     socket.on('response',function(data){
-      console.log(data);
+      $scope.gameBoard=data;
     });
+
+    $scope.gameBoard=[[[{url:'images/phoecologo.png'}],[],[],[],[]]];
 
     $scope.config = {
       group: 'test',
       sortable: false,
       animation: 300,
       filter: '.disabled',
-      onMove: function(evt){
-        console.log(evt);
-        socket.emit('sort',evt);
+      onAdd: function(evt){
+        console.log($scope.gameBoard);
+        socket.emit('sort',$scope.gameBoard);
       }
     }
   }]);
