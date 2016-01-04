fce434f07f19601803d4d4e6c6d34f64031534e8
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-01-04 16:35:49 -0600

    Modularized controller, limited connectivity to two clients, only one client can control card

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,23 +2,4 @@
  * Created by chottinger on 12/13/15.
  */
 var app = angular.module('spookyScarySkeleton', ['ng-sortable']);
-//var host =
-var socket = io.connect();
-app.controller('sortable', ['$scope',function($scope){
-    socket.on('response',function(data){
-      $scope.gameBoard=data;
-      $scope.$apply();
-    });
 
-    $scope.gameBoard= [[[{url:'images/phoecologo.png'}],[],[],[],[]]];
-
-    $scope.config = {
-      group: 'test',
-      sortable: false,
-      animation: 300,
-      filter: '.disabled',
-      onAdd: function(evt){
-        socket.emit('sort',$scope.gameBoard);
-      }
-    }
-  }]);
---
+++ public/javascripts/controllers/gameboard.js
@@ -0,0 +1,28 @@
+var socket = io.connect();
+app.controller('game', ['$scope', function($scope){
+  socket.on('response',function(data){
+    if($scope.error != 0) {
+      $scope.gameBoard = data;
+      $scope.$apply();
+    }
+  });
+
+  socket.on('oops', function(data){
+    $scope.error = data.code;
+    console.log(data.code);
+    $scope.errMessage = data.message;
+    $scope.$apply();
+  });
+
+  $scope.gameBoard= [[[{url:'images/phoecologo.png'}],[],[],[],[]]];
+
+  $scope.config = {
+    group: 'test',
+    sortable: false,
+    animation: 300,
+    filter: '.disabled',
+    onAdd: function(evt){
+      socket.emit('sort',$scope.gameBoard);
+    }
+  }
+}]);
